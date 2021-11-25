24bfa7099dd75a122fda6351a562c13c95890c68
mauro-sousa <maurosousa94@gmail.com>
2021-11-25 19:12:24 +0200

    first

---
+++ firebase.js
@@ -0,0 +1,57 @@
+
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const adminEmail = document.querySelector('#admin-email').value;
+  const addAdminRole = functions.httpsCallable('addAdminRole');
+  addAdminRole({ email: adminEmail }).then(result => {
+    console.log(result);
+  });
+});
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    user.getIdTokenResult().then(idTokenResult => {
+      user.admin = idTokenResult.claims.admin;
+      setupUI(user);
+    });
+    db.collection('guides').onSnapshot(snapshot => {
+      setupGuides(snapshot.docs);
+    }, err => console.log(err.message));
+  } else {
+    setupUI();
+    setupGuides([]);
+  }
+});
+
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('to-do.html');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+    loginForm.querySelector('.error').innerHTML = 'to-do.html';
+  }).catch(err => {
+    loginForm.querySelector('.error').innerHTML = err.message;
+  });
+});
